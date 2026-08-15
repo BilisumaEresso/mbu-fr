4be54f74911ea6369cc2fdbdcb54f4240946995d
BilisumaEresso <bilisumaere@gmail.com>
2026-08-15 20:02:25 +0300

    Fix: vite build 2

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (
-            id.includes("node_modules/react") ||
-            id.includes("node_modules/react-dom") ||
-            id.includes("node_modules/react-router-dom")
-          ) {
-            return "react-vendor";
-          }
-        },
-      },
-    },
   },
   server: {
     middlewareMode: false,
